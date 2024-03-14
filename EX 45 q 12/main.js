8b50348fbac3d10b157a04cd74f1e2483c0d47ec
Waqas Ali <160438548+alii143@users.noreply.github.com>
2024-03-15 04:33:45 +0500

    Add files via upload

---
+++ EX 45 q 12/main.js
@@ -0,0 +1,5 @@
+var members = ["talha", "hassan", "pervaiz", "umer"];
+var message = "kal kiski birthday hai:";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ EX 45 q 12/main.ts
@@ -0,0 +1,5 @@
+let members: string[] =[`talha`,`hassan`,`pervaiz`,`umer`];
+let message: string=`kal kiski birthday hai:`;
+for (let i=0; i<members.length; i++){
+   console.log(message + members[i]); 
+}
